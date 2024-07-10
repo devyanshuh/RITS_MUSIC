ae549084cdb12b237116d9fa10617ca39e8830ff
𝗪𝗔𝗥_𝗠𝗔𝗖𝗛𝗜𝗡𝗘 <devyanshu1526@gmail.com>
2024-07-10 14:33:02 +0530

    Update couples.py

--- PURVIMUSIC/plugins/tools/couples.py
+++ PURVIMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from PURVIMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="|| ˹𖤍 𝗔𝗟𝗣𝗛𝗔 𖤍˼ ||",
-            url=f"https://t.me/PURVI_UPDATES",
+            text="|| 𓆩✗𝕯𝒆𝒗𓆪 ||",
+            url=f"https://t.me/ll_war_support_ll",
         ),
     ],
 ]
